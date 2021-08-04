68adbdadcc5ec1e12dde5bc144244e1798ada196
Ryosuke.Ogawa <info@secondunit.jp>
2021-08-04 04:04:51 +0000

    register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,40 +12,47 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap" rel="stylesheet">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300&display=swap" rel="stylesheet">
     </head>
     <body>
-    <div class="login">
-        <div class="title">
+        <div class="register">
+            <div class="title">
             <h1>Share Photo</h1>
-        </div>
+            </div>
         <div class="form">
-            {!! Form::open(['route' => 'login.post']) !!}
+            {!! Form::open(['route' => 'signup.post']) !!}
                 <div class="form-group">
-                    {!! Form::label('email', 'E mail') !!}
+                    {!! Form::label('name', 'User Name') !!}
+                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                <div class="register-attention">
+                    <p>半角英数字のみ。記号は使用できません。</p>
+                </div>
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('email', 'Email') !!}
                     {!! Form::email('email', null, ['class' => 'form-control']) !!}
                 </div>
+
                 <div class="form-group">
-                    {!! Form::label('password', 'Pass word') !!}
+                    {!! Form::label('password', 'Password') !!}
                     {!! Form::password('password', ['class' => 'form-control']) !!}
                 </div>
-            <div style="text-align: right;">
-            {!! Form::submit('ログイン', ['class' => 'btn btn-outline-primary']) !!}
-            </div>
+
+                <div class="form-group">
+                    {!! Form::label('password_confirmation', 'Pass word(確認用)') !!}
+                    {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+                </div>
+                <div style="text-align: right;">
+                {!! Form::submit('登録', ['class' => 'btn btn-outline-primary']) !!}
+                </div>
             {!! Form::close() !!}
         </div>
-        <div class="attention">
-            {{-- ユーザ登録ページへのリンク --}}
-            <p>・アカウントをお持ちでない方は、{!! link_to_route('signup.get', 'こちら') !!}から登録。</p>
-            {{-- 退会へのリンク --}}
-            <p >・退会される方は、{!! link_to_route('signup.get', 'こちら') !!}。</p>
-        </div> 
     </div>
+        
     </body>
     <footer>
         <p>&copy; share photo</p>
     </footer>
-
 </html>
+    
+        
